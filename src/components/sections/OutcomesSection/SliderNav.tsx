15ed38aa259d09be829fca9eba98d9ed01f7c22e
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-07-30 18:15:03 +0300

    refactor-layout-fixes

--- src/components/sections/OutcomesSection/SliderNav.tsx
+++ src/components/sections/OutcomesSection/SliderNav.tsx
@@ -19,13 +19,7 @@ export default function SliderNav({
     <div className={s.navContainer}>
       <button className={s.arrowBtn} onClick={onPrev} aria-label="Previous slide">
         <span className={s.iconLeft}>
-          <Image
-            src="/images/icons/arrow-1.svg"
-            alt="prev"
-            width={20}
-            height={16}
-            style={{ transform: 'rotate(180deg)', width: '1.042vw', height: '0.833vw' }}
-          />
+          <Image src="/images/icons/arrow-1.svg" alt="prev" width={20} height={16} />
         </span>
       </button>
       <div className={s.dotsBlock}>
@@ -37,25 +31,11 @@ export default function SliderNav({
               <span key={idx} className={s.sliderDot} onClick={() => onDotClick(idx)}></span>
             ),
           )}
-          {/* <div
-            className={s.movingLine}
-            style={
-              {
-                '--line-x': `calc(${activeIndex} * 2.2vw)`,
-              } as React.CSSProperties
-            }
-          /> */}
         </div>
       </div>
       <button className={s.arrowBtn} onClick={onNext} aria-label="Next slide">
         <span className={s.iconRight}>
-          <Image
-            src="/images/icons/arrow-2.svg"
-            alt="next"
-            width={20}
-            height={16}
-            style={{ width: '1.042vw', height: '0.833vw' }}
-          />
+          <Image src="/images/icons/arrow-2.svg" alt="next" width={20} height={16} />
         </span>
       </button>
     </div>
