a86fbe48a300572a2b384ca67d648cf404c89e3e
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-08-07 18:31:20 +0300

    fix-icon

--- src/components/sections/OutcomesSection/OutcomesSection.tsx
+++ src/components/sections/OutcomesSection/OutcomesSection.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react'
 import s from './OutcomesSection.module.css'
-import { GlobeIcon, Globe2Icon, Chart2Icon, Analytics2Icon, Group6Icon } from '../../Icon/Icon'
+import { Group01, Analytics02, Chart03, GlobeIcon04, GlobeIcon05 } from '../../Icon/Icon'
 
 import ExpertSection from '@/components/sections/ExpertSection/ExpertSection'
 import TabSection from '@/components/ui/TabSection/TabSection'
@@ -12,27 +12,31 @@ import CustomSlider from '../../CustomSlider/CustomSlider'
 const outcomes = [
   {
     number: '01',
-    icon: <Globe2Icon />,
+    icon: <Group01 />,
+    // icon: <Globe2Icon />,
     title: 'MORE CLARITY METRICS DECISIONS',
   },
   {
     number: '02',
-    icon: <Chart2Icon />,
+    icon: <Analytics02 />,
+
+    // icon: <Chart2Icon />,
     title: 'STRUCTURE THAT REDUCES WASTE AND GUESSWORK',
   },
   {
     number: '03',
-    icon: <Analytics2Icon />,
+    // icon: <Analytics2Icon />,
+    icon: <Chart03 />,
     title: 'SMARTER CAMPAIGNS ALIGNED WITH REAL BUSINESS GOALS',
   },
   {
     number: '04',
-    icon: <Group6Icon />,
+    icon: <GlobeIcon04 />,
     title: 'BETTER ROI THROUGH CONTINUOUS TESTING',
   },
   {
     number: '05',
-    icon: <GlobeIcon />,
+    icon: <GlobeIcon05 />,
     title: 'LESS STRESS, MORE FOCUS FOR YOUR TEAM',
   },
 ]
