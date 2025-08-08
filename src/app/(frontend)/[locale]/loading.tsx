677d30070614d0df7d71b5b4d78cb52b321a9d78
projection-agency <projection.agency@gmail.com>
2025-08-09 02:47:47 +0300

    Remove commented-out GridBackground component and Loading function from loading.tsx

--- src/app/(frontend)/[locale]/loading.tsx
+++
@@ -1,9 +0,0 @@
-// import GridBackground from '@/components/GridBackground/GridBackground'
-
-// export default function Loading() {
-//   return (
-//     <div style={{ minHeight: '100vh', background: '#222' }}>
-//       <GridBackground />
-//     </div>
-//   )
-// }
