b9f0cef5f3e9e74f4196cb69415783408cff84fb
projection-agency <projection.agency@gmail.com>
2025-08-11 20:15:16 +0300

    Implement dynamic metadata generation for locale-based content in layout.tsx

---
+++ src/app/(frontend)/[locale]/loading.tsx
@@ -0,0 +1,13 @@
+export default function Loading() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    ></div>
+  )
+}
